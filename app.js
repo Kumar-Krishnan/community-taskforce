4c12e839e7893e38004c9ea60a52c36939b4fecf
PsijicSeht <38445096+PsijicSeht@users.noreply.github.com>
2018-06-07 15:51:02 -0400

    app using method override

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-
+const methodOverride = require('method-override')
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const mongoose = require('mongoose')
@@ -14,7 +14,7 @@ const citiesController = require('./routes/citiesController')
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
-
+app.use(methodOverride('_method'))
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
