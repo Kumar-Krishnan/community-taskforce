5d7b1389b816fdaf97a7246327d96243fbcf58e9
PsijicSeht <38445096+PsijicSeht@users.noreply.github.com>
2018-06-08 11:07:43 -0400

    completed issues route and template indexpage

--- routes/issuesController.js
+++ routes/issuesController.js
@@ -1,5 +1,4 @@
 const express = require('express')
-const router = express.Router()
 const router = express.Router({ mergeParams: true })
 const City = require('../models/City')
 const Issue = require('../models/Issue')
@@ -10,8 +9,13 @@ const Issue = require('../models/Issue')
 router.get('/', (req,res, next)=>{
     City.findOne({name:req.params.cityName})
     .then((city)=>{
+        const cityName = city.name
         const issues = city.issues
-        res.render('issues/index', {issues})
+        res.render('issues/index', 
+        {
+            issues:issues,
+            cityName: cityName
+        })
     })
 })
 
