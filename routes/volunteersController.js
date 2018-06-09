07244b70e8b18b05d07ba8c92915a91655644832
PsijicSeht <38445096+PsijicSeht@users.noreply.github.com>
2018-06-09 17:36:43 -0400

    successfully sent volunteers array to index

--- routes/volunteersController.js
+++ routes/volunteersController.js
@@ -2,13 +2,18 @@ const express = require('express')
 const router = express.Router({ mergeParams: true })
 const City = require('../models/City')
 const Issue = require('../models/Issue')
+const Volunteer = require('../models/Volunteer')
 
 
 router.get('/', (req,res)=>{
     cityName = req.params.cityName
     issueIndex = req.params.issueIndex
-
-    res.redirect(`/cities/${cityName}/issues/${issueIndex}/hello/boyashaka`)
+    City.findOne({name:req.params.cityName})
+    .then((city)=>{
+        volunteers = city.issues[issueIndex].volunteers
+        res.send(`${volunteers}`)
+    })
+   
 })
 
 module.exports=router
