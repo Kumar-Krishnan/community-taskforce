4a37208b7217203d0272cbe48f51b64f64c69e47
PsijicSeht <38445096+PsijicSeht@users.noreply.github.com>
2018-06-09 18:05:10 -0400

    completed template volunteer show route

--- routes/volunteersController.js
+++ routes/volunteersController.js
@@ -8,12 +8,24 @@ const Volunteer = require('../models/Volunteer')
 router.get('/', (req,res)=>{
     cityName = req.params.cityName
     issueIndex = req.params.issueIndex
-    City.findOne({name:req.params.cityName})
+    City.findOne({name: cityName})
     .then((city)=>{
         volunteers = city.issues[issueIndex].volunteers
         res.render('volunteers/index', {volunteers,cityName,issueIndex})
     })
    
+})
+
+router.get('/:volunteer', (req,res)=>{
+    cityName = req.params.cityName
+    issueIndex = req.params.issueIndex
+    volunteerIndex = req.params.volunteerIndex
+
+    City.findOne({name: cityName})
+    .then((city)=>{
+        volunteers = city.issues[issueIndex].volunteers[volunteerIndex]
+        res.render('volunteers/show')
+    })
 })
 
 module.exports=router
