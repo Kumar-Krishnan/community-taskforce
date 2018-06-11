6f40296418ece2c3a9a6d7eb3a5fd035ed8f7e6e
PsijicSeht <38445096+PsijicSeht@users.noreply.github.com>
2018-06-11 15:40:38 -0400

    created taskForce schema

---
+++ routes/taskforcesController.js
@@ -0,0 +1,12 @@
+const Schema = require('mongoose').Schema
+
+const taskforceSchema = new Schema({
+    name: String,
+    missionStatement: String,
+    leadOrganizer: String,
+    contactEmail: String,
+    skillsPreferred: [String]
+    // agents: [agentSchema]
+})
+
+module.exports = volunteerSchema
